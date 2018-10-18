71523bef5cb01688f929886c5d2555739d100d7d
Travis Koehring <travis.koehring@gmail.com>
2018-10-18 18:57:29 -0500

    Image analysis part 3

--- lineDetection.py
+++ lineDetection.py
@@ -15,9 +15,10 @@ def houghLines(img, rhoR, thetaR):
     cos_t = np.cos(thetas)
     sin_t = np.sin(thetas)
     num_thetas = len(thetas)
-    accumulator = np.zeros((2*diag, num_thetas), dtype=np.uint8)
-    y_idx, x_idx = np.nonzero(img)
+    accumulator = np.zeros((2*diag, num_thetas), dtype=np.uint64)
 
+    y_idx, x_idx = np.nonzero(img)
+    pointLoc = np.zeros((x_idx.shape[0], thetas.size), dtype=np.uint64)
     xSize = x_idx.shape[0]
 
     i = 0
@@ -30,9 +31,10 @@ def houghLines(img, rhoR, thetaR):
         while t_idx < num_thetas:
             rho = int(round(x * cos_t[t_idx] + y * sin_t[t_idx])) + diag
             accumulator[rho, t_idx] += 1
+            pointLoc[i, t_idx] = rho
             t_idx += 1
         i += 1
-    return accumulator, thetas, diag
+    return accumulator, thetas, x_idx, y_idx, pointLoc, diag
 
 def plot_hough_acc(accumulator, diag, thetas, plot_title='Hough Accumulator Plot'):
     ''' A function that plot a Hough Space using Matplotlib. '''
@@ -57,15 +59,20 @@ def newImg():
     return gray, img, height, width
 
 def reset():
+    img = cv.imread(imgPath, cv.IMREAD_COLOR)
     return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 def help():
-    tkMessageBox.showinfo("Help", 'Listed below are the key functions:\n'
+    tkMessageBox.showinfo("Help", 'You must find the lines before running commands p, i, w, s.\n'
+                                  'Listed below are the key functions:\n'
+                                  'h - Open help dialog\n'
                                   'g - Show grayscale image\n'
-                                  'x - Show hough lines\n'
-                                  'p - Show hough parameter space plot\n'
                                   'z - Load new image\n'
-                                  'h - Open help dialog\n')
+                                  'x - Find lines\n'
+                                  'p - Show hough parameter space plot\n'
+                                  'i - Show points used in voting\n'
+                                  'w - Show hough lines with line fitting\n'
+                                  's - Show hough lines\n')
 
 
 imgPath = tkFileDialog.askopenfilename()
@@ -105,7 +112,7 @@ while(True):
         peakT = cv.getTrackbarPos('Peak Threshold', 'Image')
 
         edges = cv.Canny(gray, hMin, hMax, apertureSize=3)
-        accumulator, thetas, diag = houghLines(edges, rhoR, thetaR)
+        accumulator, thetas, x_idx, y_idx, pointLoc, diag = houghLines(edges, rhoR, thetaR)
         temp = accumulator.flatten()
 
         j = 0
@@ -116,6 +123,45 @@ while(True):
             j += 1
 
         vals = np.vstack(np.unravel_index(idx, accumulator.shape)).T
+
+        i = 0
+        temp = []
+        while i < vals.shape[0]:
+            j = 0
+            points = []
+            while j < pointLoc.shape[0]:
+                if pointLoc[j, [vals[i][1]]] == vals[i][0]:
+                    img[y_idx[j], x_idx[j]] = [0, 255, 0]
+                    points.append((x_idx[j], y_idx[j], 1))
+                j += 1
+            temp.append((list(points)))
+            i += 1
+        cv.imshow('Image', img)
+
+        i = 0
+        while i < vals.shape[0]:
+            points = np.array(temp[i])
+
+            j = 0
+            sum = np.zeros((3, 3))
+            while j < points.shape[0]:
+                sum += np.matmul(points[j], points[j].transpose())
+                j += 1
+            w, v = np.linalg.eig(sum)
+
+            x0 = points[0][0]
+            y0 = points[0][1]
+            a = v[-1][0]
+            b = v[-1][1]
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            cv.line(gray, (x1, y1), (x2, y2), (0, 0, 255), 1)
+            i += 1
+
+        bestFit = gray.copy()
+        gray = reset()
 
         i = 0
         while i < vals.shape[0]:
@@ -130,9 +176,16 @@ while(True):
             x2 = int(x0 - 1000 * (-b))
             y2 = int(y0 - 1000 * (a))
             cv.line(gray, (x1, y1), (x2, y2), (0, 0, 255), 1)
-            i = i + 1
+            i += 1
 
-        cv.imshow('Image', gray)
+        houghImg = gray.copy()
+        cv.imshow('Image', bestFit)
+    elif key == ord('i'):
+        cv.imshow('Image', img)
+    elif key == ord('w'):
+        cv.imshow('Image', bestFit)
+    elif key == ord('s'):
+        cv.imshow('Image', houghImg)
     elif key == ord('z'):
         gray, img, height, width = newImg()
 
